292ee40835e559d2e2349c3c4dc2b73e844183ac
Dušan Juretić <dusan.juretic@gmail.com>
2012-05-15 23:19:38 -0400

    fixed spelling typos

--- lib/chinese_vocab/modules/helper_methods.rb
+++ lib/chinese_vocab/modules/helper_methods.rb
@@ -10,7 +10,7 @@ module Chinese
     def is_unicode?(word)
       # Remove all non-ascii and non-unicode word characters
       word = distinct_words(word).join
-      # English text at this point only contains characters that are mathed by \w
+      # English text at this point only contains characters that are matched by \w
       # Chinese text at this point contains mostly/only unicode word characters that are not matched by \w.
       # In case of Chinese text the size of 'char_arr' therefore has to be smaller than the size of 'word'
       char_arr = word.scan(/\w/)
@@ -18,7 +18,7 @@ module Chinese
     end
 
     # Input: "除了。。。 以外。。。"
-    # Outout: ["除了", "以外"]
+    # Output: ["除了", "以外"]
     def distinct_words(word)
       # http://stackoverflow.com/a/3976004
       # Alternative: /[[:word:]]+/
